9fa56a9f5f02f6301d6b293436d227c48741a378
ajay singh <ajaypratap1705374@gmail.com>
2022-07-15 14:26:45 +0530

    first commit

---
+++ src/models/schema.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const itemSchema=new mongoose.Schema({
+    id:{
+        type:Number
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    stage:{
+        type:Number
+    }
+})
+const monmodel=new mongoose.model("items",itemSchema)
+module.exports=MensRanking;
