81b86084370ff783e5e09fb993b97632ebc38a61
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2022-07-21 12:06:01 +0530

    Update Counter.js

--- Project 1/src/models/Counter.js
+++ Project 1/src/models/Counter.js
@@ -1,1 +1,10 @@
-
+const counterSchema={
+    id:{
+        type:String
+    },
+    seq:{
+        type:Number
+    }
+}
+const countermodel=new mongoose.model("counter",counterSchema)
+module.exports=countermodel
